2c30c754d039757433c9dd6818741bad9665fdcd
Wojciech <wlodarczyk.woj@gmail.com>
2019-01-21 15:59:14 +0100

    Fixed problem with game not reseting its state on start

--- game_logic.py
+++ game_logic.py
@@ -21,11 +21,11 @@ character_mapping = {
 
 
 game = {
-   "finished": False,
-   "scores": 0,
-   "map": "",
-   "x": 1,
-   "y": 1,
+    "finished": False,
+    "scores": 0,
+    "map": "",
+    "x": 1,
+    "y": 1,
 }
 
 
@@ -33,23 +33,23 @@ def read_map(file_name):
     lines = open(file_name).read().split('\n')
     raw_map = []
     for line in lines:
-       row = []
-       for c in line:
-           row.append(c)
-       if len(row) > 1:
-           raw_map.append(row)
+        row = []
+        for c in line:
+            row.append(c)
+        if len(row) > 1:
+            raw_map.append(row)
     return raw_map
 
 
 def print_map():
-   """Cleans the map and prints current map on the screen."""
-   clear()
-   print("Scores:", game["scores"])
-   for line in game["map"]:
-       for c in line:
-           map_c = character_mapping[c]
-           print(map_c, end="")
-       print()
+    """Cleans the map and prints current map on the screen."""
+    clear()
+    print("Scores:", game["scores"])
+    for line in game["map"]:
+        for c in line:
+            map_c = character_mapping[c]
+            print(map_c, end="")
+        print()
 
 
 def move_player(move_x, move_y):
@@ -73,26 +73,29 @@ def move_player(move_x, move_y):
 
 
 def handle_input():
-   player_input = get_key()
+    player_input = get_key()
 
-   if player_input == "up":
-       move_player(-1, 0)
-   elif player_input == "down":
-       move_player(1, 0)
-   elif player_input == "right":
-       move_player(0, 1)
-   elif player_input == "left":
-       move_player(0, -1)
-   elif player_input == "exit":
-       return False
-   return True
+    if player_input == "up":
+        move_player(-1, 0)
+    elif player_input == "down":
+        move_player(1, 0)
+    elif player_input == "right":
+        move_player(0, 1)
+    elif player_input == "left":
+        move_player(0, -1)
+    elif player_input == "exit":
+        return False
+    return True
 
 
 def start_game(file_name):
-   game["map"] = read_map(file_name)
+    game["x"] = 1
+    game["y"] = 1
+    game["finished"] = False
+    game["map"] = read_map(file_name)
 
-   while not game["finished"]:
-       print_map()
-       result = handle_input()
-       if result == False:
-           break
+    while not game["finished"]:
+        print_map()
+        result = handle_input()
+        if result == False:
+            break
