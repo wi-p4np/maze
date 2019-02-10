61dddb6442879aedcd9018991ec5bf0fd7ccd1b7
Elena Greci <elena.greci1@gmail.com>
2019-02-10 18:09:02 +0100

    Modified highscore_board (2 new functions read and save scores into csv file) modified menu (reads csv and prints 10 best scores) new file highscore_board.csv extra: modified column maps to add some traps and doors

--- high_score.py
+++ high_score.py
@@ -1,13 +1,40 @@
 from datetime import datetime
 highscore_list = []
+highscore = []
+HIGHSCORE_BOARD = './highscore_board.csv'
+
 
 def add_highscore(map_name, score):
+    global highscore
     highscore = tuple([map_name, str(datetime.now().date()), score])
     highscore_list.append(highscore)
     highscore_list.sort(key=lambda x: x[2], reverse=True)
+    save_highscore()
     return highscore_list
 
 
 def print_highscore():
-    for i in highscore_list:
-        print(str(i) + '\n')
+    print("10 top scores")
+    for i in highscore_list[:10]:  # prints out only 10 best scores
+        print('map: ' + str(i[0]) + ' | ' + 'date: ' + str(i[1]) +
+              ' | ' + 'score: ' + str(i[2]))
+
+
+def save_highscore():
+    global highscore
+    file = open(HIGHSCORE_BOARD, 'a')
+    file.write(str(highscore[0]) + ',' + str(highscore[1]) + ',' +
+               str(highscore[2]) + '\n')
+    file.close()
+
+
+def read_highscore():
+    global highscore_list
+    lines = open(HIGHSCORE_BOARD).readlines()[1:]
+    for line in lines:
+        line = line.strip().split(',')
+        highscore = tuple([str(line[0]), str(line[1]), int(line[2])])
+        highscore_list.append(highscore)
+        highscore_list.sort(key=lambda x: x[2], reverse=True)
+    return highscore_list
+
